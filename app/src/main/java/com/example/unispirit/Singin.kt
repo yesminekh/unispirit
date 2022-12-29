822670b513b9dc11a0ecf728098b62c2de9110c2
yesminekh <73406102+yesminekh@users.noreply.github.com>
2022-12-29 17:45:08 +0100

    first commit

---
+++ app/src/main/java/com/example/unispirit/Singin.kt
@@ -0,0 +1,11 @@
+package com.example.unispirit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Singin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_singin)
+    }
+}
